7356d9d7f29b645b2538864068aff2a054afa719
serg <zaswed76@gmail.com>
2016-06-28 15:56:08 +0300

    add game admin

--- mindimages/games/admin/base_admin.py
+++ mindimages/games/admin/base_admin.py
@@ -2,10 +2,8 @@
 # -*- coding: utf-8 -*-
 
 
-import sys
-import os
-from PyQt5 import QtWidgets
-root = os.path.join(os.path.dirname(__file__))
 
+import os
+root = os.path.join(os.path.dirname(__file__))
 tool_button = os.path.join(root, 'resouces/icons/base_tool.png')
 from ..admin.gui import base
